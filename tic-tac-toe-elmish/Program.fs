de4ea5dc14c29baadc3396a723be8706b206a0cb
Yang Qu <52755+yang-qu@users.noreply.github.com>
2023-12-24 00:06:28 +1000

    Game board - Feliz style

--- tic-tac-toe-elmish/Program.fs
+++ tic-tac-toe-elmish/Program.fs
@@ -1,2 +1,120 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Feliz
+open Elmish
+open Elmish.React
+open Fable.Core.JsInterop
+
+importSideEffects "./styles.css"
+
+type State = { Count: int }
+
+type Msg =
+    | Increment
+    | Decrement
+
+let init() = { Count = 0 }, Cmd.none
+
+let update (msg: Msg) (state: State) =
+    match msg with
+    | Increment -> { state with Count = state.Count + 1 }, Cmd.none
+    | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
+
+let render (state: State) (dispatch: Msg -> unit) =
+    Html.div [
+        prop.className "game"
+        prop.children [
+            Html.div [
+                prop.className "game-board"
+                prop.children [
+                    Html.div [
+                        prop.className "status"
+                        prop.text "Next player: X"
+                    ]
+                    Html.div [
+                        prop.className "board-row"
+                        prop.children [
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                        ]
+                    ]
+                    Html.div [
+                        prop.className "board-row"
+
+                        prop.children [
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                        ]
+                    ]
+                    Html.div [
+                        prop.className "board-row"
+
+                        prop.children [
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                            Html.button [
+                                prop.className "square"
+                                prop.text "X"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.className "game-info"
+                prop.children [
+                    Html.ol [
+                        Html.li [
+                            prop.children [
+                                Html.button [
+                                    prop.text "Go to game start"
+                                ]
+                            ]
+                        ]
+                        Html.li [
+                            prop.children [
+                                Html.button [
+                                    prop.text "Go to move #1"
+                                ]
+                            ]
+                        ]
+                        Html.li [
+                            prop.children [
+                                Html.button [
+                                    prop.text "Go to move #2"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+Program.mkProgram init update render
+|> Program.withReactBatched "root"
+|> Program.run
--- tic-tac-toe-elmish/Program.fs.js
+++ tic-tac-toe-elmish/Program.fs.js
@@ -1,4 +1,97 @@
-import { printf, toConsole } from "./fable_modules/fable-library.4.9.0/String.js";
+import { Union, Record } from "./fable_modules/fable-library.4.9.0/Types.js";
+import { union_type, record_type, int32_type } from "./fable_modules/fable-library.4.9.0/Reflection.js";
+import { Cmd_none } from "./fable_modules/Fable.Elmish.4.0.0/cmd.fs.js";
+import { createElement } from "react";
+import { createObj } from "./fable_modules/fable-library.4.9.0/Util.js";
+import { Interop_reactApi } from "./fable_modules/Feliz.2.7.0/./Interop.fs.js";
+import { singleton, ofArray } from "./fable_modules/fable-library.4.9.0/List.js";
+import { ProgramModule_mkProgram, ProgramModule_run } from "./fable_modules/Fable.Elmish.4.0.0/program.fs.js";
+import { Program_withReactBatched } from "./fable_modules/Fable.Elmish.React.4.0.0/react.fs.js";
+import "./styles.css";
 
-toConsole(printf("Hello from F#"));
 
+export class State extends Record {
+    constructor(Count) {
+        super();
+        this.Count = (Count | 0);
+    }
+}
+
+export function State_$reflection() {
+    return record_type("Program.State", [], State, () => [["Count", int32_type]]);
+}
+
+export class Msg extends Union {
+    constructor(tag, fields) {
+        super();
+        this.tag = tag;
+        this.fields = fields;
+    }
+    cases() {
+        return ["Increment", "Decrement"];
+    }
+}
+
+export function Msg_$reflection() {
+    return union_type("Program.Msg", [], Msg, () => [[], []]);
+}
+
+export function init() {
+    return [new State(0), Cmd_none()];
+}
+
+export function update(msg, state) {
+    if (msg.tag === 1) {
+        return [new State(state.Count - 1), Cmd_none()];
+    }
+    else {
+        return [new State(state.Count + 1), Cmd_none()];
+    }
+}
+
+export function render(state, dispatch) {
+    let elems_8, elems_3, elems, elems_1, elems_2, elems_7, children, elems_4, elems_5, elems_6;
+    return createElement("div", createObj(ofArray([["className", "game"], (elems_8 = [createElement("div", createObj(ofArray([["className", "game-board"], (elems_3 = [createElement("div", {
+        className: "status",
+        children: "Next player: X",
+    }), createElement("div", createObj(ofArray([["className", "board-row"], (elems = [createElement("button", {
+        className: "square",
+        children: "X",
+    }), createElement("button", {
+        className: "square",
+        children: "X",
+    }), createElement("button", {
+        className: "square",
+        children: "X",
+    })], ["children", Interop_reactApi.Children.toArray(Array.from(elems))])]))), createElement("div", createObj(ofArray([["className", "board-row"], (elems_1 = [createElement("button", {
+        className: "square",
+        children: "X",
+    }), createElement("button", {
+        className: "square",
+        children: "X",
+    }), createElement("button", {
+        className: "square",
+        children: "X",
+    })], ["children", Interop_reactApi.Children.toArray(Array.from(elems_1))])]))), createElement("div", createObj(ofArray([["className", "board-row"], (elems_2 = [createElement("button", {
+        className: "square",
+        children: "X",
+    }), createElement("button", {
+        className: "square",
+        children: "X",
+    }), createElement("button", {
+        className: "square",
+        children: "X",
+    })], ["children", Interop_reactApi.Children.toArray(Array.from(elems_2))])])))], ["children", Interop_reactApi.Children.toArray(Array.from(elems_3))])]))), createElement("div", createObj(ofArray([["className", "game-info"], (elems_7 = [(children = ofArray([createElement("li", createObj(singleton((elems_4 = [createElement("button", {
+        children: "Go to game start",
+    })], ["children", Interop_reactApi.Children.toArray(Array.from(elems_4))])))), createElement("li", createObj(singleton((elems_5 = [createElement("button", {
+        children: "Go to move #1",
+    })], ["children", Interop_reactApi.Children.toArray(Array.from(elems_5))])))), createElement("li", createObj(singleton((elems_6 = [createElement("button", {
+        children: "Go to move #2",
+    })], ["children", Interop_reactApi.Children.toArray(Array.from(elems_6))]))))]), createElement("ol", {
+        children: Interop_reactApi.Children.toArray(Array.from(children)),
+    }))], ["children", Interop_reactApi.Children.toArray(Array.from(elems_7))])])))], ["children", Interop_reactApi.Children.toArray(Array.from(elems_8))])])));
+}
+
+ProgramModule_run(Program_withReactBatched("root", ProgramModule_mkProgram(init, update, render)));
+
+//# sourceMappingURL=Program.fs.js.map
