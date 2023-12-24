b4c5aeb91909d00441011f4c3dca67daff7f6dd8
Yang Qu <52755+yang-qu@users.noreply.github.com>
2023-12-24 22:47:24 +1000

    Game board - Feliz style

--- tic-tac-toe-elmish/Program.fs
+++ tic-tac-toe-elmish/Program.fs
@@ -11,7 +11,7 @@ type Msg =
     | Increment
     | Decrement
 
-let init() = { Count = 0 }, Cmd.none
+let init () = { Count = 0 }, Cmd.none
 
 let update (msg: Msg) (state: State) =
     match msg with
@@ -19,101 +19,40 @@ let update (msg: Msg) (state: State) =
     | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
-    Html.div [
-        prop.className "game"
-        prop.children [
-            Html.div [
-                prop.className "game-board"
-                prop.children [
-                    Html.div [
-                        prop.className "status"
-                        prop.text "Next player: X"
-                    ]
-                    Html.div [
-                        prop.className "board-row"
-                        prop.children [
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                        ]
-                    ]
-                    Html.div [
-                        prop.className "board-row"
+    Html.div
+        [ prop.className "game"
+          prop.children
+              [ Html.div
+                    [ prop.className "game-board"
+                      prop.children
+                          [ Html.div [ prop.className "status"; prop.text "Next player: X" ]
+                            Html.div
+                                [ prop.className "board-row"
+                                  prop.children
+                                      [ Html.button [ prop.className "square"; prop.text "" ]
+                                        Html.button [ prop.className "square"; prop.text "" ]
+                                        Html.button [ prop.className "square"; prop.text "" ] ] ]
+                            Html.div
+                                [ prop.className "board-row"
 
-                        prop.children [
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                        ]
-                    ]
-                    Html.div [
-                        prop.className "board-row"
+                                  prop.children
+                                      [ Html.button [ prop.className "square"; prop.text "" ]
+                                        Html.button [ prop.className "square"; prop.text "" ]
+                                        Html.button [ prop.className "square"; prop.text "" ] ] ]
+                            Html.div
+                                [ prop.className "board-row"
 
-                        prop.children [
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                            Html.button [
-                                prop.className "square"
-                                prop.text "X"
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-            Html.div [
-                prop.className "game-info"
-                prop.children [
-                    Html.ol [
-                        Html.li [
-                            prop.children [
-                                Html.button [
-                                    prop.text "Go to game start"
-                                ]
-                            ]
-                        ]
-                        Html.li [
-                            prop.children [
-                                Html.button [
-                                    prop.text "Go to move #1"
-                                ]
-                            ]
-                        ]
-                        Html.li [
-                            prop.children [
-                                Html.button [
-                                    prop.text "Go to move #2"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-    ]
+                                  prop.children
+                                      [ Html.button [ prop.className "square"; prop.text "" ]
+                                        Html.button [ prop.className "square"; prop.text "" ]
+                                        Html.button [ prop.className "square"; prop.text "" ] ] ] ] ]
+                Html.div
+                    [ prop.className "game-info"
+                      prop.children
+                          [ Html.ol
+                                [ Html.li [ prop.children [ Html.button [ prop.text "Go to game start" ] ] ]
+                                  Html.li [ prop.children [ Html.button [ prop.text "Go to move #1" ] ] ]
+                                  Html.li [ prop.children [ Html.button [ prop.text "Go to move #2" ] ] ] ] ] ] ] ]
 
 Program.mkProgram init update render
 |> Program.withReactBatched "root"
