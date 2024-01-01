7cbae472d6abd9c510c2756ec5d61ae83d82d8ca
Yang Qu <52755+yang-qu@users.noreply.github.com>
2024-01-01 15:37:29 +1000

    Initial JSX example commit

---
+++ fable-jsx/Program.fs
@@ -0,0 +1,6 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Browser
+
+let div = document.createElement "div"
+div.innerHTML <- "Hello world!"
+document.body.appendChild div |> ignore
