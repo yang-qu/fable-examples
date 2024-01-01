fc5f572722b32c6eefb12179bdfeadaef6493499
Yang Qu <52755+yang-qu@users.noreply.github.com>
2024-01-01 17:40:01 +1000

    fable-jsx src

--- fable-jsx/Program.fs
+++
@@ -1,6 +0,0 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open Browser
-
-let div = document.createElement "div"
-div.innerHTML <- "Hello world!"
-document.body.appendChild div |> ignore
