040db8f583772e4eddd522b174e663c50e2184e3
Yang Qu <52755+yang-qu@users.noreply.github.com>
2024-01-02 15:35:18 +1000

    date pickers jsx initial commit

---
+++ fable-jsx-x-date-pickers/src/Program.fs
@@ -0,0 +1,16 @@
+﻿module JsxDatePickers
+
+open Fable.Core
+open Browser
+open Feliz
+open Fable.Core.JsInterop
+
+[<JSX.Component>]
+let DatePickers () =
+    JSX.jsx 
+        $"""
+        <p>hello world</p>
+        """
+
+let root = ReactDOM.createRoot (document.getElementById "root")
+root.render(DatePickers() |> toReact)
