aac07a55ca55a909121e74800386295b7f3804e8
Yang Qu <52755+yang-qu@users.noreply.github.com>
2024-01-08 22:30:27 +1000

    Feliz MUI button binding

---
+++ feliz-3rd-party-react-library/src/Program.fs
@@ -0,0 +1,24 @@
+﻿module FelizMuiButton
+
+open Feliz
+open Fable.Core.JsInterop
+
+let muiButton: obj = importDefault "@mui/material/Button"
+
+type MuiButton =
+    static member inline Variant (v: string) = "variant" ==> v
+    static member inline Children (c: #seq<ReactElement>) = "children" ==>  (c:> ReactElement seq)
+
+    static member inline create props = Interop.reactApi.createElement (muiButton, createObj !!props)
+
+[<ReactComponent>]
+let ButtonView() =
+    MuiButton.create [
+        MuiButton.Variant "contained"
+        MuiButton.Children [(Html.text "Next")]
+    ]
+
+open Browser.Dom
+
+let root = ReactDOM.createRoot(document.getElementById "root")
+root.render(ButtonView())
